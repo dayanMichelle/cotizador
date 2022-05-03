5812d87895663d32936f88c55aba03f25b1ddbf9
Dayan Arango <dayaara6@gmail.com>
2022-05-03 16:14:04 -0400

    terminado

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,7 +1,45 @@
 import useCotizador from "../hooks/useCotizados";
+import { MARCAS, PLANES } from "../constants";
+import { useCallback,useMemo,useRef } from "react";
+
 const Resultado = () => {
-  const { resultado } = useCotizador();
-  return <div>{resultado ==0 ? null: resultado}</div>;
+  const { resultado, datos } = useCotizador();
+  const { marca, year, plan } = datos;
+  const yearRef = useRef(year);
+  const [nombreMarca] = useMemo(() =>
+    MARCAS.filter((m) => m.id === Number(marca)),
+    [resultado]
+  );
+  const [nombrePlan] =  useMemo(() =>
+    PLANES.filter((p) => p.id === Number(plan)),
+    [resultado]
+    );
+  console.log(nombreMarca);
+  if (resultado === 0) return null;
+
+  return (
+    <div className="bg-gray-100 text-center mt-5 p-5 shadow">
+      <h2 className="text-gray-600 font-black text-3xl">Resumen</h2>
+      <p className="my-2">
+        <span className="font-bold">Marca:</span>
+        {nombreMarca.nombre}
+      </p>
+
+      <p className="my-2">
+        <span className="font-bold">Plan:</span>
+        {nombrePlan.nombre}
+      </p>
+      <p className="my-2">
+        <span className="font-bold">Año del auto:</span>
+        {yearRef.current}
+      </p>
+
+      <p className="my-2 text-2xl">
+        <span className="font-bold">Total Cotización:</span>
+        {resultado}
+      </p>
+    </div>
+  );
 };
 
 export default Resultado;
